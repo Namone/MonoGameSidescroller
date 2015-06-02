aaaca21a5ebaa46d08a237e8fb2713a368617d28
Jean <jeanoliveirabr1@gmail.com>
2015-06-02 00:36:56 -0300

    Created the project and the class Entity.

---
+++ SideScroller/SideScroller/Entity.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+namespace SideScroller
+{
+    class Entity
+    {
+        public int Health;
+        public bool isAlive;
+        public Texture2D EntityTexture;
+        public Vector2 Position;
+
+        public int Width 
+        {
+            get { return EntityTexture.Width; }
+        }
+
+        public int Height
+        {
+            get { return EntityTexture.Height; }
+        }
+    }
+}
