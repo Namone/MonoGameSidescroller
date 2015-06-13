0e28e744d8cfb38ccde0b1f08d16ac0eac359fe6
JeanAraujo <jeanoliveirabr1@gmail.com>
2015-06-13 00:37:24 -0300

    Minor updates

--- SideScroller/SideScroller/Entity.cs
+++ SideScroller/SideScroller/Entity.cs
@@ -14,7 +14,7 @@ namespace SideScroller
         public int Health;
         public Texture2D Texture;
         public Vector2 Position;
-
+		public Rectangle Rect;
         public void Draw(SpriteBatch spriteBatch)
         {
             spriteBatch.Draw(this.Texture, this.Position, null, Color.White, 0f, Vector2.Zero, 1f, SpriteEffects.None, 0f);
